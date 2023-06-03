f512e2added000909345dee389d84302a1e38e48
Nikhil Borse <nikhilrb13@gmail.com>
2023-06-03 10:44:21 +0530

    Working of Javascript Assignment

---
+++ WorkingOfJavascript Assignment/Assignment02.js
@@ -0,0 +1,7 @@
+// Write a function called "multiplyNumbers" that takes two numbers as arguments and returns their
+// product. Use function expressions to define the function and call the function before it is declared to
+// demonstrate hoisting.
+
+console.log(multiplyNumbers(3, 6))
+
+const multiplyNumbers = (a, b) => a * b;
---
+++ WorkingOfJavascript Assignment/Assignment04.js
@@ -0,0 +1,17 @@
+// Declare three variables, one using let, one using var, and one using const, all inside a block scope. Assign
+// them values and log their values to the console before and after they are declared to demonstrate variable
+// hoisting.
+
+{
+    console.log(x)
+    console.log(y)
+    console.log(z)
+
+    var x = 'Good'
+    let x = 'Morning'
+    const z = '!!!'
+
+    console.log(x)
+    console.log(y)
+    console.log(z)
+}
